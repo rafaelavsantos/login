26308ce134a58c2312d3b4b0d03b1c2a332c7a6e
Rafaela Santos <rafaelasantosminios@gmail.com>
2024-05-06 15:40:51 -0300

    feat: add default layout style

--- src/app/components/layout-login/layout-login.component.ts
+++ src/app/components/layout-login/layout-login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-layout-login',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './layout-login.component.scss'
 })
 export class LayoutLoginComponent {
-
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondBtnText: string = "";
 }
