3af1b8f5f98f8e49ce959bb77fbcea279c407fac
Rafaela Santos <rafaelasantosminios@gmail.com>
2024-05-09 10:14:10 -0300

    feat: Implement the login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { LayoutLoginComponent } from '../../components/layout-login/layout-login.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LayoutLoginComponent],
+  imports: [LayoutLoginComponent, ReactiveFormsModule, PrimaryInputComponent],
+  providers: [LoginService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastrService: ToastrService,
+  ){
+    this.loginForm = new FormGroup( {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  submit() {
+    // Efetuar o login unindo com o backend
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastrService.success("Login feito com sucesso!"),
+      error: () => this.toastrService.error("Erro inesperado! Tente novamente mais tarde."),
+    })
+  }
+
+  navigate() {
+    this.router.navigate(["signup"])
+  }
 }
