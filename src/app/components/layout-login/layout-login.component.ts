b8f3b264370d36173b829b5e4bc6ef08ee7109fc
Rafaela Santos <rafaelasantosminios@gmail.com>
2024-05-09 10:12:08 -0300

    feat: Implement strutuc of layout login

--- src/app/components/layout-login/layout-login.component.ts
+++ src/app/components/layout-login/layout-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-layout-login',
@@ -11,4 +11,15 @@ export class LayoutLoginComponent {
   @Input() title: string = "";
   @Input() primaryBtnText: string = "";
   @Input() secondBtnText: string = "";
+  @Input() disablePrimaryBtn: boolean = true;
+  @Output("submit") onSubmit = new EventEmitter();
+  @Output("navigate") onNavigate = new EventEmitter();
+
+  submit() {
+    this.onSubmit.emit();
+  }
+
+  navigate() {
+    this.onNavigate.emit();
+  }
 }
